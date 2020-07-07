4ae8f4311351ad05a373c3517793d082980747ba
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-07 20:57:08 -0300

    feat(feat/user-service): setup user controller in Main

--- user-service/src/main/kotlin/Main.kt
+++ user-service/src/main/kotlin/Main.kt
@@ -1,11 +1,25 @@
 package com.lorenzoog.gitkib.userservice
 
+import com.lorenzoog.gitkib.userservice.controllers.userController
+import com.lorenzoog.gitkib.userservice.database.DatabaseService
+import com.lorenzoog.gitkib.userservice.database.impls.PostgresService
+import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, commandLineEnvironment(args))
+    val environment = dotenv()
+
+    // The service that will connect to the database
+    val databaseService: DatabaseService = PostgresService()
+    val database = databaseService.connect(environment)
+
+    // Setup application controllers/routes
+    server.application.apply {
+        userController(database)
+    }
 
     server.start()
 }
---
+++ user-service/src/main/kotlin/controllers/UserController.kt
@@ -0,0 +1,11 @@
+package com.lorenzoog.gitkib.userservice.controllers
+
+import io.ktor.application.Application
+import io.ktor.routing.routing
+import org.jetbrains.exposed.sql.Database
+
+fun Application.userController(database: Database) {
+    routing {
+
+    }
+}
