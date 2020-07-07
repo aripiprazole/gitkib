8e9b7df365d4702271730a558d41a299d3ca20c9
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-07 20:20:25 -0300

    feat(user-service): add user-service

---
+++ user-service/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization") version "1.3.70"
+}
+
+group = "com.lorenzoog.gitkib"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor","ktor-server-netty", "1.3.2")
+
+    testImplementation("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ user-service/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package com.lorenzoog.gitkib.userservice
+
+import io.ktor.server.engine.commandLineEnvironment
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, commandLineEnvironment(args))
+
+    server.start()
+}
