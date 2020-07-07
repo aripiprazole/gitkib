fea0b321d6c15818cc50ca86e2b01ecee74385f4
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-07 20:23:15 -0300

    chore(feat/user-service): add exposed

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -15,6 +15,11 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("io.ktor","ktor-server-netty", "1.3.2")
 
+    // Exposed dependencies
+    implementation("org.jetbrains.exposed", "exposed-core", "0.24.1")
+    implementation("org.jetbrains.exposed", "exposed-dao", "0.24.1")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.24.1")
+
     testImplementation("junit", "junit", "4.12")
 }
 
