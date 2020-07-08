577638f2da7924ae885a748efa3cd4ef8dba041a
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-07 21:40:33 -0300

    style(feat/user-service): fix controllers ident

--- user-service/src/main/kotlin/controllers/UserController.kt
+++ user-service/src/main/kotlin/controllers/UserController.kt
@@ -1,11 +1,15 @@
 package com.lorenzoog.gitkib.userservice.controllers
 
 import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
 import io.ktor.routing.routing
 import org.jetbrains.exposed.sql.Database
 
 fun Application.userController(database: Database) {
-    routing {
+  routing {
 
-    }
+  }
 }
