3f84d585195f38363c39e0e902a757a5121ac7db
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-07 22:06:27 -0300

    fix(feat/user-service): fix duplicate column in UserTable: name, it should be email

--- user-service/src/main/kotlin/controllers/UserController.kt
+++ user-service/src/main/kotlin/controllers/UserController.kt
@@ -1,5 +1,7 @@
 package com.lorenzoog.gitkib.userservice.controllers
 
+import com.lorenzoog.gitkib.userservice.entities.User
+import com.lorenzoog.gitkib.userservice.tables.UserTable
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
@@ -7,9 +9,17 @@ import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Op
+import org.jetbrains.exposed.sql.select
+
+const val PAGINATION_OFFSET = 15
 
 fun Application.userController(database: Database) {
   routing {
+    get("users") {
+      val users = User.all().limit(PAGINATION_OFFSET)
 
+      call.respond(HttpStatusCode.OK, users)
+    }
   }
 }
