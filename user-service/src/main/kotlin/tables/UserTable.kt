39fa2b28617134516d671f8e439a6af9fb5a3c41
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-07 21:39:31 -0300

    style(feat/user-service): fix tables ident

--- user-service/src/main/kotlin/tables/UserTable.kt
+++ user-service/src/main/kotlin/tables/UserTable.kt
@@ -4,8 +4,8 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 
 object UserTable : LongIdTable(name = "users") {
 
-    val username = varchar("name", 32)
-    val email = varchar("name", 32)
-    val password = varchar("password", 72)
+  val username = varchar("name", 32)
+  val email = varchar("name", 32)
+  val password = varchar("password", 72)
 
 }
