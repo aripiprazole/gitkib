0774f75e8cc7ed0d275a0ed91cf32d99182d8e3b
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-07 21:39:16 -0300

    style(feat/user-service): fix Main.kt ident

--- user-service/src/main/kotlin/Main.kt
+++ user-service/src/main/kotlin/Main.kt
@@ -4,22 +4,32 @@ import com.lorenzoog.gitkib.userservice.controllers.userController
 import com.lorenzoog.gitkib.userservice.database.DatabaseService
 import com.lorenzoog.gitkib.userservice.database.impls.PostgresService
 import io.github.cdimascio.dotenv.dotenv
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
 import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.text.DateFormat
 
 fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, commandLineEnvironment(args))
-    val environment = dotenv()
+  val server = embeddedServer(Netty, commandLineEnvironment(args))
+  val environment = dotenv()
 
-    // The service that will connect to the database
-    val databaseService: DatabaseService = PostgresService()
-    val database = databaseService.connect(environment)
+  // The service that will connect to the database
+  val databaseService: DatabaseService = PostgresService()
+  val database = databaseService.connect(environment)
 
-    server.start()
+  server.start()
 
-    // Setup application controllers/routes
-    server.application.apply {
-        userController(database)
+  // Setup application controllers/routes and other services
+  server.application.apply {
+    install(ContentNegotiation) {
+      jackson {
+        dateFormat = DateFormat.getDateInstance()
+      }
     }
+
+    userController(database)
+  }
 }
