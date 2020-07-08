de9daab5601b6996d3965c7ed04b3cbd48103a1a
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-07 21:40:01 -0300

    style(feat/user-service): fix database services ident

--- user-service/src/main/kotlin/database/impls/PostgresService.kt
+++ user-service/src/main/kotlin/database/impls/PostgresService.kt
@@ -10,16 +10,16 @@ import javax.sql.DataSource
 
 class PostgresService : DatabaseService {
 
-    override fun connect(environment: Dotenv): Database {
-        val config = HikariConfig().apply {
-            jdbcUrl = environment["DB_URL"] ?: throw IllegalStateException("The database jdbc url can not be null")
-            username = environment["DB_USERNAME"] ?: throw IllegalStateException("The database username can not be null")
-            password = environment["DB_PASSWORD"] ?: throw IllegalStateException("The database password can not be null")
-        }
-
-        val dataSource: DataSource = HikariDataSource(config)
-
-        return Database.connect(dataSource)
+  override fun connect(environment: Dotenv): Database {
+    val config = HikariConfig().apply {
+      jdbcUrl = environment["DB_URL"] ?: throw IllegalStateException("The database jdbc url can not be null")
+      username = environment["DB_USERNAME"] ?: throw IllegalStateException("The database username can not be null")
+      password = environment["DB_PASSWORD"] ?: throw IllegalStateException("The database password can not be null")
     }
+
+    val dataSource: DataSource = HikariDataSource(config)
+
+    return Database.connect(dataSource)
+  }
 
 }
