b6f5ec2a64643c8fe839fe31340bc91008ab41f1
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-09 01:13:19 -0300

    fix(commons/database): set email an unique index

--- commons/src/main/kotlin/database/tables/UserTable.kt
+++ commons/src/main/kotlin/database/tables/UserTable.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 object UserTable : LongIdTable(name = "users") {
 
   val username = varchar("name", 32)
-  val email = varchar("email", 32)
+  val email = varchar("email", 32).uniqueIndex()
   val password = varchar("password", 72)
 
 }
