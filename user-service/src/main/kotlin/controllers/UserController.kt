10fefc7791f03738480ce905b427af596c6b2708
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-09 00:10:53 -0300

    feat(feat/user-service): add userController@index

--- user-service/src/main/kotlin/controllers/UserController.kt
+++ user-service/src/main/kotlin/controllers/UserController.kt
@@ -1,13 +1,26 @@
 package com.lorenzoog.gitkib.userservice.controllers
 
+import com.lorenzoog.gitkib.commons.database.entities.User
+import com.lorenzoog.gitkib.commons.database.utils.paginate
+import io.ktor.application.call
+import io.ktor.response.respond
 import io.ktor.routing.*
+import io.ktor.util.KtorExperimentalAPI
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 const val PAGINATION_OFFSET = 15
 
+@KtorExperimentalAPI
 fun Route.userController(database: Database) {
   get("users") {
-    // TODO
+    val page = call.request.queryParameters["page"] ?: "0"
+
+    newSuspendedTransaction(db = database) {
+      val users = User.all().paginate(page.toInt(), PAGINATION_OFFSET)
+
+      call.respond(users)
+    }
   }
 
   get("users/{id}") {
