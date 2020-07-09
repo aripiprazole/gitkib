7eebee089a45e3d7e7eba6ad485e3d8f2dc6791e
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-08 23:37:33 -0300

    refactor(feat/user-service): remove creating database in user service main

--- commons/src/main/kotlin/Main.kt
+++ commons/src/main/kotlin/Main.kt
@@ -1,1 +1,14 @@
 package com.lorenzoog.gitkib.commons
+
+import com.lorenzoog.gitkib.commons.database.tables.UserTable
+import org.jetbrains.exposed.sql.SchemaUtils
+
+/**
+ * Run common actions for the project
+ *
+ * @return [Unit]
+ */
+fun main() {
+  // Creating project tables
+  SchemaUtils.create(UserTable)
+}
--- user-service/src/main/kotlin/Main.kt
+++ user-service/src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ package com.lorenzoog.gitkib.userservice
 
 import com.lorenzoog.gitkib.commons.database.DatabaseService
 import com.lorenzoog.gitkib.commons.database.impls.PostgresService
-import com.lorenzoog.gitkib.commons.database.tables.UserTable
 import com.lorenzoog.gitkib.userservice.controllers.userController
 import io.github.cdimascio.dotenv.dotenv
 import io.ktor.application.install
@@ -12,8 +11,6 @@ import io.ktor.routing.routing
 import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
 import java.text.DateFormat
 
 fun main(args: Array<String>) {
@@ -23,13 +20,6 @@ fun main(args: Array<String>) {
   // The service that will connect to the database
   val databaseService: DatabaseService = PostgresService()
   val database = databaseService.connect(environment)
-
-  // Create database.entities' database.tables
-  transaction(database) {
-    SchemaUtils.create(
-      UserTable
-    )
-  }
 
   server.start()
 
