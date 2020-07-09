dccf075ebb0c0b09010f972d97d31ef5eddb5260
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-08 23:42:46 -0300

    refactor(feat/user-service): delegate specific function to project createTables

--- commons/src/main/kotlin/Main.kt
+++ commons/src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 package com.lorenzoog.gitkib.commons
 
+import com.lorenzoog.gitkib.commons.database.impls.PostgresService
 import com.lorenzoog.gitkib.commons.database.tables.UserTable
+import io.github.cdimascio.dotenv.dotenv
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
  * Run common actions for the project
@@ -9,6 +12,16 @@ import org.jetbrains.exposed.sql.SchemaUtils
  * @return [Unit]
  */
 fun main() {
-  // Creating project tables
+  createTables()
+}
+
+/**
+ * Create project tables
+ *
+ * @return [Unit]
+ */
+fun createTables() = transaction(
+  PostgresService().connect(dotenv())
+) {
   SchemaUtils.create(UserTable)
 }
