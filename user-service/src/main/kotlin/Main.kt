70916114f6a4c565c26268ca3ad2fb7ca6e2f45f
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-09 23:14:13 -0300

    refactor(feat/user-service): isolate app settings

--- user-service/src/main/kotlin/Main.kt
+++ user-service/src/main/kotlin/Main.kt
@@ -1,16 +1,6 @@
 package com.lorenzoog.gitkib.userservice
 
-import com.lorenzoog.gitkib.commons.database.DatabaseService
-import com.lorenzoog.gitkib.commons.database.entities.User
-import com.lorenzoog.gitkib.commons.database.impls.PostgresService
-import com.lorenzoog.gitkib.commons.database.repositories.Repository
-import com.lorenzoog.gitkib.commons.database.repositories.UserRepository
-import com.lorenzoog.gitkib.userservice.controllers.userController
-import io.github.cdimascio.dotenv.dotenv
-import io.ktor.application.install
-import io.ktor.features.ContentNegotiation
-import io.ktor.gson.gson
-import io.ktor.routing.routing
+import io.ktor.application.Application
 import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -19,24 +9,11 @@ import io.ktor.util.KtorExperimentalAPI
 @KtorExperimentalAPI
 fun main(args: Array<String>) {
   val server = embeddedServer(Netty, commandLineEnvironment(args))
-  val environment = dotenv()
-
-  // The service that will connect to the database
-  val databaseService: DatabaseService = PostgresService()
-  val database = databaseService.connect(environment)
-
-  val userRepository: Repository<Long, User> = UserRepository(database)
 
   server.start()
 
   // Setup application controllers/routes and other services
-  server.application.apply {
-    install(ContentNegotiation) {
-      gson()
-    }
-
-    routing {
-      userController(database, userRepository)
-    }
-  }
+  server.application
+    .apply(Application::setup)
+    .apply(Application::controllers)
 }
