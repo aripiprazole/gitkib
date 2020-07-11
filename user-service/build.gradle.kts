f8a18760de3c6c88c53025b6da5a71e646ebee09
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-11 03:38:25 -0300

    test(feat/user-service): test UserController@index

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -31,6 +31,9 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.postgresql:postgresql")
 
+  testImplementation("io.mockk:mockk:1.10.0")
+  testImplementation("junit:junit")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
