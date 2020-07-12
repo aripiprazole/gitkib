2e28639963e68e246ef80d95db29e2b23de3f081
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-12 04:06:55 -0300

    fix(feat/user-service): add remaining spring security dependency

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-data-rest")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
