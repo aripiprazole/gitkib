e9a2e77d2fd3c42b3f345587fcee4ca6bfe42584
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-11 21:21:10 -0300

    chore(feat/user-service): add remaining dependencies

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -25,6 +25,11 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+  implementation("org.hibernate:hibernate-core:5.4.18.Final")
+  implementation("org.hibernate:hibernate-entitymanager:5.4.18.Final")
+
+  implementation("javassist:javassist:3.12.1.GA")
+
   implementation("org.hibernate:hibernate-validator:5.1.0.Final")
 
   implementation("com.zaxxer:HikariCP:3.4.5")
