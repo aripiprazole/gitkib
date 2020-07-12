337c4c6ef7b1f9e43e0483416753a5b3b101c924
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-11 22:40:52 -0300

    feat(feat/user-service): setup consul discovery and health checker

--- user-service/src/main/kotlin/UserServiceApplication.kt
+++ user-service/src/main/kotlin/UserServiceApplication.kt
@@ -3,13 +3,25 @@ package com.lorenzoog.gitkib.userservice
 import com.lorenzoog.gitkib.userservice.discovery.setupConsulDiscovery
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.TaskScheduler
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 
 @SpringBootApplication
 @Configuration
 @EnableJpaRepositories("com.lorenzoog.gitkib.userservice.repositories")
-class UserServiceApplication
+@EnableAsync
+@EnableScheduling
+class UserServiceApplication {
+
+  @Bean("healthChecker")
+  fun healthChecker(): TaskScheduler = ThreadPoolTaskScheduler()
+
+}
 
 fun main(args: Array<String>) {
   setupConsulDiscovery()
