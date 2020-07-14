340be18e120262c26c094eb00c0b1ea1b87435ff
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-14 11:09:46 -0300

    refactor(feat/user-service): remove useless imports in SecurityConfig.kt

--- user-service/src/main/kotlin/security/SecurityConfig.kt
+++ user-service/src/main/kotlin/security/SecurityConfig.kt
@@ -10,11 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.http.HttpMethod.GET
 import org.springframework.http.HttpMethod.POST
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
