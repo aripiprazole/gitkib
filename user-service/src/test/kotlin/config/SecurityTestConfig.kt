77c57ea624ede13abb0d2ae918ce7bdc3a707c66
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-14 01:45:57 -0300

    fix(feat/user-service): fix tests and test with permissions

--- user-service/src/test/kotlin/config/SecurityTestConfig.kt
+++ user-service/src/test/kotlin/config/SecurityTestConfig.kt
@@ -1,11 +1,23 @@
 package com.lorenzoog.gitkib.userservice.config
 
+import com.lorenzoog.gitkib.userservice.entities.Privilege
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import kotlin.reflect.full.declaredMemberProperties
+
+object DefaultUsers {
+  const val JUST_LOGGED = "just-logged"
+  const val ALL_PERMISSIONS = "all-permissions"
+}
+
+private val allPermissions = Privilege.Companion::class.declaredMemberProperties.map {
+  SimpleGrantedAuthority(it.get(Privilege.Companion).toString())
+}
 
 @TestConfiguration
 class SecurityTestConfig {
@@ -13,11 +25,14 @@ class SecurityTestConfig {
   @Bean
   @Primary
   fun userDetailsService(): UserDetailsService {
-    val user = User("just-logged", "fake password", emptyList())
+    val password = "fake password"
 
-    return InMemoryUserDetailsManager(listOf(
-      user
-    ))
+    val users = listOf(
+      User(DefaultUsers.JUST_LOGGED, password, emptyList()),
+      User(DefaultUsers.ALL_PERMISSIONS, password, allPermissions)
+    )
+
+    return InMemoryUserDetailsManager(users)
   }
 
 }
