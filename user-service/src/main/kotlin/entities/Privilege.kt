accca47a0024d92aa89160316cd801175053f247
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-14 01:57:14 -0300

    refactor(feat/user-service): change default permissions names

--- user-service/src/main/kotlin/entities/Privilege.kt
+++ user-service/src/main/kotlin/entities/Privilege.kt
@@ -21,10 +21,10 @@ data class Privilege(
    * Default privileges
    */
   companion object {
-    const val VIEW_USER = "@users/view"
-    const val CREATE_USER = "@users/create"
-    const val UPDATE_USER = "@users/update"
-    const val DELETE_USER = "@users/destroy"
+    const val VIEW_USER = "users.view"
+    const val CREATE_USER = "users.create"
+    const val UPDATE_USER = "users.update"
+    const val DELETE_USER = "users.destroy"
   }
 
 }
