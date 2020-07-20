78194cbb40c6bfe8c4b60d69fc5560118f5e6c5f
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-20 10:22:54 -0300

    fix(user-service): remove jpa annotations

--- user-service/src/main/kotlin/UserServiceApplication.kt
+++ user-service/src/main/kotlin/UserServiceApplication.kt
@@ -2,11 +2,9 @@ package com.lorenzoog.gitkib.userservice
 
 import com.lorenzoog.gitkib.userservice.discovery.setupConsulDiscovery
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.TaskScheduler
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -15,8 +13,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 
 @SpringBootApplication
 @Configuration
-@EnableJpaRepositories("com.lorenzoog.gitkib.userservice.repositories")
-@EntityScan("com.lorenzoog.gitkib.userservice.entities")
 @EnableAsync
 @EnableScheduling
 @EnableGlobalMethodSecurity(securedEnabled = true)
