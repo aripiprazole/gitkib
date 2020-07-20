bd10f153126e0737b2f73aebb2860c24144db308
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-20 06:54:42 -0300

    fix(user-service): add default values for the sized iterables and create user_role table

--- user-service/src/test/kotlin/utils/factories.kt
+++ user-service/src/test/kotlin/utils/factories.kt
@@ -4,6 +4,7 @@ import com.lorenzoog.gitkib.userservice.entities.Privilege
 import com.lorenzoog.gitkib.userservice.entities.Profile
 import com.lorenzoog.gitkib.userservice.entities.Role
 import com.lorenzoog.gitkib.userservice.entities.User
+import org.jetbrains.exposed.sql.SizedCollection
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun User.Companion.mock(block: User.() -> Unit = {}) = transaction {
@@ -11,18 +12,21 @@ fun User.Companion.mock(block: User.() -> Unit = {}) = transaction {
     username = "Jubiscreudi"
     password = "nou32h79"
     email = "email@org.com"
+    roles = SizedCollection()
   }.apply(block)
 }
 
 fun Role.Companion.mock(block: Role.() -> Unit = {}) = transaction {
   Role.new {
     name = "Cargo fuck :sunglasses:"
+    privileges = SizedCollection()
   }.apply(block)
 }
 
 fun Privilege.Companion.mock(block: Privilege.() -> Unit = {}) = transaction {
   Privilege.new {
     name = "pode.dar.o.cu"
+    roles = SizedCollection()
   }.apply(block)
 }
 
