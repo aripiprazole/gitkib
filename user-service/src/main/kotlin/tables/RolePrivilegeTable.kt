9e0ad6f757eb200eb9b48f7e82ed0ca32a65d434
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-20 16:01:40 -0300

    refactor(user-service): change name of RolePrivilegeTable.kt to role_privilege

--- user-service/src/main/kotlin/tables/RolePrivilegeTable.kt
+++ user-service/src/main/kotlin/tables/RolePrivilegeTable.kt
@@ -3,7 +3,7 @@ package com.lorenzoog.gitkib.userservice.tables
 import org.jetbrains.exposed.sql.ReferenceOption.CASCADE
 import org.jetbrains.exposed.sql.Table
 
-object RolePrivilegeTable : Table(name = "user_profile") {
+object RolePrivilegeTable : Table(name = "role_privilege") {
 
   val role = reference(name = "role", foreign = RoleTable, onDelete = CASCADE)
   val privilege = reference(name = "privilege", foreign = PrivilegeTable, onDelete = CASCADE)
