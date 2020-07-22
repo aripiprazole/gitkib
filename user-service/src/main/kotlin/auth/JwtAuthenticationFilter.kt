7e76158716c9ff7abbfa7283be20b5c632f05e5a
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-22 18:27:11 -0300

    refactor(user-service): create config package and move auth package to the root package

--- user-service/src/main/kotlin/auth/JwtAuthenticationFilter.kt
+++ user-service/src/main/kotlin/auth/JwtAuthenticationFilter.kt
@@ -1,15 +1,13 @@
-package com.lorenzoog.gitkib.userservice.security.auth
+package com.lorenzoog.gitkib.userservice.auth
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
-import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
-import org.springframework.stereotype.Component
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
--- user-service/src/main/kotlin/configs/SecurityConfig.kt
+++ user-service/src/main/kotlin/configs/SecurityConfig.kt
@@ -1,10 +1,9 @@
-package com.lorenzoog.gitkib.userservice.security
+package com.lorenzoog.gitkib.userservice.configs
 
 import com.auth0.jwt.algorithms.Algorithm
-import com.lorenzoog.gitkib.userservice.controllers.AuthController
 import com.lorenzoog.gitkib.userservice.controllers.AppProfileController
-import com.lorenzoog.gitkib.userservice.security.auth.JwtAuthenticationFilter
-import com.lorenzoog.gitkib.userservice.security.auth.UsernameUserDetailsService
+import com.lorenzoog.gitkib.userservice.auth.JwtAuthenticationFilter
+import com.lorenzoog.gitkib.userservice.auth.UsernameUserDetailsService
 import com.lorenzoog.gitkib.userservice.services.UserProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
