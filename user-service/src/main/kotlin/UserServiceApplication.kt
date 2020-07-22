0783d8f6d983d78791e84fb36f25f595f1533b72
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-22 18:42:57 -0300

    refactor(user-service): define healthChecker bean in ApplicationConfig

--- user-service/src/main/kotlin/UserServiceApplication.kt
+++ user-service/src/main/kotlin/UserServiceApplication.kt
@@ -4,13 +4,10 @@ import com.lorenzoog.gitkib.userservice.configs.defaultBeans
 import com.lorenzoog.gitkib.userservice.discovery.setupConsulDiscovery
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kotlin.coroutine.EnableCoroutine
-import org.springframework.scheduling.TaskScheduler
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 
 @SpringBootApplication
@@ -19,12 +16,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableScheduling
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableCoroutine
-class UserServiceApplication {
-
-  @Bean("healthChecker")
-  fun healthChecker(): TaskScheduler = ThreadPoolTaskScheduler()
-
-}
+class UserServiceApplication
 
 fun main(args: Array<String>) {
   setupConsulDiscovery()
--- user-service/src/main/kotlin/configs/ApplicationConfig.kt
+++ user-service/src/main/kotlin/configs/ApplicationConfig.kt
@@ -4,6 +4,8 @@ import com.lorenzoog.gitkib.userservice.auth.UsernameUserDetailsService
 import com.lorenzoog.gitkib.userservice.services.UserProvider
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.beans
+import org.springframework.scheduling.TaskScheduler
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.security.core.userdetails.UserDetailsService
 
 val defaultBeans = beans {
@@ -11,6 +13,10 @@ val defaultBeans = beans {
 
   bean<UserDetailsService>("userDetailService") {
     UsernameUserDetailsService(ref())
+  }
+
+  bean<TaskScheduler>("healthChecker") {
+    ThreadPoolTaskScheduler()
   }
 }
 
