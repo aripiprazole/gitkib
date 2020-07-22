94669efcb79660297120805cc12f87dd57fcf77f
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-22 20:35:18 -0300

    feat(user-service): inject coroutine scope

--- user-service/src/main/kotlin/configs/ApplicationConfig.kt
+++ user-service/src/main/kotlin/configs/ApplicationConfig.kt
@@ -2,11 +2,17 @@ package com.lorenzoog.gitkib.userservice.configs
 
 import com.lorenzoog.gitkib.userservice.auth.UsernameUserDetailsService
 import com.lorenzoog.gitkib.userservice.services.UserProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.beans
 import org.springframework.scheduling.TaskScheduler
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.security.core.userdetails.UserDetailsService
+
+val job = Job()
+val coroutineScope = CoroutineScope(job + Dispatchers.Default)
 
 val defaultBeans = beans {
   bean<UserProvider>()
@@ -18,6 +24,8 @@ val defaultBeans = beans {
   bean<TaskScheduler>("healthChecker") {
     ThreadPoolTaskScheduler()
   }
+
+  bean { coroutineScope }
 }
 
 @Configuration
