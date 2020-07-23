d1779699eeb3ceb7172b01496d8e9f4b191ca8b9
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-23 07:19:23 -0300

    refactor(user-service): remove useless imports in ApplicationConfig.kt

--- user-service/src/main/kotlin/configs/ApplicationConfig.kt
+++ user-service/src/main/kotlin/configs/ApplicationConfig.kt
@@ -1,11 +1,9 @@
 package com.lorenzoog.gitkib.userservice.configs
 
 import com.lorenzoog.gitkib.userservice.auth.UsernameUserDetailsService
-import com.lorenzoog.gitkib.userservice.services.UserProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.beans
 import org.springframework.scheduling.TaskScheduler
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
