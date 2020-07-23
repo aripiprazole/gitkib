4e65684afa5b99a043b087905386344e3eaf51cd
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-23 07:18:45 -0300

    fix(user-service): remove the spring kotlin coroutine EnableCoroutine annotation from the app

--- user-service/src/main/kotlin/UserServiceApplication.kt
+++ user-service/src/main/kotlin/UserServiceApplication.kt
@@ -5,7 +5,6 @@ import com.lorenzoog.gitkib.userservice.discovery.setupConsulDiscovery
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Configuration
-import org.springframework.kotlin.coroutine.EnableCoroutine
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
@@ -15,7 +14,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableAsync
 @EnableScheduling
 @EnableGlobalMethodSecurity(securedEnabled = true)
-@EnableCoroutine
 class UserServiceApplication
 
 fun main(args: Array<String>) {
