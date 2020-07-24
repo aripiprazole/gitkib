c5d42206718b9865d5ea85ef0ba208f5be41d635
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-24 19:28:38 -0300

    feat(user-service): add user controller

--- user-service/src/main/kotlin/config.kt
+++ user-service/src/main/kotlin/config.kt
@@ -1,20 +1,15 @@
 package com.lorenzoog.gitkib.userservice
 
-import kotlinx.coroutines.flow.flowOf
+import com.lorenzoog.gitkib.userservice.controllers.userController
 import org.springframework.context.support.beans
 import org.springframework.web.reactive.function.server.RouterFunctions
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.bodyAndAwait
 import org.springframework.web.reactive.function.server.coRouter
 
 val defaultBeans = beans {
   bean("webHandler") {
     RouterFunctions.toWebHandler(
       coRouter {
-        GET("/") {
-          ServerResponse.ok()
-            .bodyAndAwait(flowOf("Hello, world"))
-        }
+        userController(ref())
       }
     )
   }
