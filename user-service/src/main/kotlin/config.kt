b17b93234b68c949c060868870899eb7b4dede1a
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-24 19:30:48 -0300

    fix(user-service): inject UserService

--- user-service/src/main/kotlin/config.kt
+++ user-service/src/main/kotlin/config.kt
@@ -1,11 +1,14 @@
 package com.lorenzoog.gitkib.userservice
 
 import com.lorenzoog.gitkib.userservice.controllers.userController
+import com.lorenzoog.gitkib.userservice.services.UserService
 import org.springframework.context.support.beans
 import org.springframework.web.reactive.function.server.RouterFunctions
 import org.springframework.web.reactive.function.server.coRouter
 
 val defaultBeans = beans {
+  bean<UserService>()
+
   bean("webHandler") {
     RouterFunctions.toWebHandler(
       coRouter {
