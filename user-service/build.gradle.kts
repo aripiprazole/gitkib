285587b635a54080fcc75804c192721be9627783
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-31 22:15:57 -0300

    feat(user-service): remove spring

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,9 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("io.spring.dependency-management") version "1.0.9.RELEASE"
   kotlin("jvm") version "1.3.72"
-  kotlin("plugin.spring") version "1.3.72"
 }
 
 group = "com.lorenzoog.gitkib"
@@ -14,35 +12,16 @@ java.sourceCompatibility = JavaVersion.VERSION_1_8
 repositories {
   mavenCentral()
   jcenter()
-  maven("https://dl.bintray.com/konrad-kaminski/maven")
-}
-
-@Suppress("NOTHING_TO_INLINE")
-inline fun exposed(dependency: String, version: String = "0.24.1") = "org.jetbrains.exposed:exposed-$dependency:$version"
-
-dependencyManagement {
-  imports {
-    mavenBom("org.springframework.boot:spring-boot-dependencies:2.3.1.RELEASE")
-  }
 }
 
 dependencies {
   implementation("io.arrow-kt:arrow-core:0.10.4")
   implementation("io.arrow-kt:arrow-syntax:0.10.4")
 
-  implementation("io.netty:netty-transport-native-epoll:4.1.16.Final")
-
   // kotlin
   arrayOf("reflect", "stdlib-jdk8").forEach {
     implementation(kotlin(it))
   }
-
-  // spring
-  arrayOf("webflux", "context", "web", "core").forEach {
-    implementation("org.springframework:spring-$it")
-  }
-
-  implementation("io.projectreactor.netty:reactor-netty:0.9.10.RELEASE")
 
   implementation("org.slf4j:slf4j-api:1.7.30")
   implementation("ch.qos.logback:logback-classic:1.2.3")
@@ -54,16 +33,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-$it:1.3.7")
   }
 
-  // spring kotlin coroutines
-  arrayOf("kotlin-coroutine", "webflux-kotlin-coroutine").forEach {
-    implementation("org.springframework.kotlin:spring-$it:0.3.7")
-  }
-
   implementation("com.zaxxer:HikariCP:3.4.5")
 
   // exposed
   arrayOf("jdbc", "dao", "core").forEach {
-    implementation(exposed(it))
+    implementation("org.jetbrains.exposed:exposed-$it:0.24.1")
   }
 
   implementation("com.orbitz.consul:consul-client:1.4.0")
@@ -79,9 +53,8 @@ dependencies {
 
   // test
   testImplementation("junit:junit")
-  testImplementation("io.projectreactor:reactor-test")
   testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.mockito:mockito-all:1.10.19")
 }
 
 tasks.withType<Test> {
--- user-service/src/test/kotlin/factories/Factory.kt
+++
@@ -1,9 +0,0 @@
-package com.lorenzoog.gitkib.userservice.tests.factories
-
-interface Factory<T> {
-
-  fun createMany(amount: Int): Set<T>
-
-  fun createOne(builder: T.() -> Unit = {}): T
-
-}
