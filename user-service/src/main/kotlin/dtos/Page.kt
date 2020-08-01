cde95789df4fa7c738e32e2a9d4fa547e0858298
LorenzooG <lorenzooguimaraes@gmail.com>
2020-07-31 22:15:57 -0300

    feat(user-service): remove spring

--- user-service/src/main/kotlin/dtos/Page.kt
+++
@@ -1,7 +0,0 @@
-package com.lorenzoog.gitkib.userservice.dtos
-
-data class Page<T>(
-  val content: List<T>,
-  val total: Int,
-  val currentPage: Int
-)
--- user-service/src/main/kotlin/tables/Users.kt
+++
@@ -1,9 +0,0 @@
-package com.lorenzoog.gitkib.userservice.tables
-
-import org.jetbrains.exposed.dao.id.LongIdTable
-
-object Users : LongIdTable("users") {
-  val username = varchar("username", 32)
-  val email = varchar("email", 32)
-  val password = varchar("password", 32)
-}
