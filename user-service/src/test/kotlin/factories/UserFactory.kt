c8fca31ac2741eb4b8c85329e5ef0694f4764a82
LorenzooG <lorenzooguimaraes@gmail.com>
2020-08-03 11:09:30 -0300

    test(user-service): add user factory to test Feature("user-rest-api")

---
+++ user-service/src/test/kotlin/factories/UserFactory.kt
@@ -0,0 +1,27 @@
+package com.lorenzoog.gitkib.userservice.tests.factories
+
+import com.lorenzoog.gitkib.userservice.entities.User
+import org.jetbrains.exposed.sql.transactions.transaction
+
+/**
+ * TODO: add faker library
+ */
+class UserFactory : IFactory<User> {
+  override fun createOne(builder: User.() -> Unit) = transaction {
+    User.new {
+      username = "Fake username"
+      email = "Fake email"
+      password = "Fake password"
+    }.apply(builder)
+  }
+
+  override fun createMany(amount: Int, builder: User.() -> Unit) =
+    (1..amount)
+      .map { createOne(builder) }
+
+  fun createWithPermissions(permissions: List<String>, builder: User.() -> Unit = {}) =
+      createOne(builder).also {
+        TODO("Not yet implemented the permission system!")
+      }
+
+}
