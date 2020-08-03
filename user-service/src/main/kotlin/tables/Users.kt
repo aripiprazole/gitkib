d0e94f5ab90bebcb02a8ba21e13447c68f5f8e54
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-03 11:10:31 -0300

    feat(user-service): add user service, table, entity and update dto

---
+++ user-service/src/main/kotlin/tables/Users.kt
@@ -0,0 +1,9 @@
+package com.lorenzoog.gitkib.userservice.tables
+
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object Users : LongIdTable() {
+  val username = varchar("username", 32)
+  val email = varchar("email", 24)
+  val password = text("password")
+}
