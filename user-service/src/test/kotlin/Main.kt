6fafec8423558e649114e4c2b0ddbde82cb15048
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-05 02:48:53 -0300

    feat(user-service): add builder that can mock instead of only pass the environment variables

--- user-service/src/test/kotlin/Main.kt
+++ user-service/src/test/kotlin/Main.kt
@@ -3,33 +3,45 @@ package com.lorenzoog.gitkib.userservice.tests
 import com.lorenzoog.gitkib.userservice.module
 import io.ktor.application.Application
 import io.ktor.config.MapApplicationConfig
-import io.ktor.server.engine.ApplicationEngine
-import io.ktor.server.engine.applicationEngineEnvironment
-import io.ktor.server.engine.connector
-import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.*
 import io.ktor.server.netty.Netty
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import org.kodein.di.DI
+import org.kodein.di.ktor.di
 import org.slf4j.LoggerFactory
 
 private val job = Job()
 private val scope = CoroutineScope(job + Dispatchers.Default)
 
+class TestApplicationBuilder(private val app: Application) {
+  fun kodein(builder: DI.MainBuilder.() -> Unit) {
+    app.di(builder)
+  }
+
+  @OptIn(KtorExperimentalAPI::class)
+  fun config(builder: MapApplicationConfig.() -> Unit) {
+    (app.environment.config as MapApplicationConfig).apply(builder)
+  }
+}
+
 @KtorExperimentalAPI
 fun createApplication(
   host: String = "localhost",
   port: Int = 8000,
-  env: MapApplicationConfig.() -> Unit
+  builder: TestApplicationBuilder.() -> Unit = {}
 ): ApplicationEngine = embeddedServer(Netty, applicationEngineEnvironment {
   parentCoroutineContext = scope.coroutineContext
   log = LoggerFactory.getLogger("ktor.application")
   watchPaths = emptyList()
 
-  config = MapApplicationConfig().apply(env)
+  module {
+    TestApplicationBuilder(this).apply(builder)
 
-  module(Application::module)
+    module()
+  }
 
   connector {
     this.port = port
